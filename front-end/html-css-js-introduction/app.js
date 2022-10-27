462a71a264cd99a05790f5c5355548ba14a52fc7
Dima Ipihin <dimaepikhin@gmail.com>
2022-10-27 21:29:50 +0300

    js sumDigits function HW # 11

--- front-end/html-css-js-introduction/app.js
+++ front-end/html-css-js-introduction/app.js
@@ -1,24 +1,15 @@
-// function sumDigits(number) {
-//     if (number < 0)
-//         number = -number;
-//     let rem = 0, sum = 0;
-//     do {
-//         rem = number % 10;
-//         sum = sum + rem;
-//         number = number / 10;
-
-//     } while (number != 0);
-//     return sum;
-// }
-// console.log(sumDigits(-123));
-
-
-function sumDigits2(number) {
-    if (number < 0)
+function sumDigits(number) {
+    if (number < 0){
         number = -number;
-    let str = String(number);
-    let sum = 0;
-    for (let i = 0; i < str.length; i++) sum += Number(str[i]);
-    return sum;
 }
-console.log(sumDigits2(-123))
+let sum = 0;
+while(number > 0){
+sum += number % 10;
+number = Math.floor(number/10);
+}
+return sum;
+}
+console.log(sumDigits(-1111))
+
+
+
