50176a7f519d4471a541d80fffe984c21a8a2986
Dima Ipihin <dimaepikhin@gmail.com>
2022-10-30 18:56:06 +0200

    Hw #11

--- front-end/html-css-js-introduction/app.js
+++ front-end/html-css-js-introduction/app.js
@@ -9,7 +9,19 @@ function sumDigits(number) {
     }
     return sum;
 }
-console.log(sumDigits(-1111))
+console.log('sum =', sumDigits(123))
+////////////////////////////////////////////
+function sumDigits1(number) {
 
+    number = Math.abs(number);
+    let sum = 0;
+    number = Math.trunc(number);
+    do {
+        let digit = number % 10;
+        number = (number - digit) / 10;
+        sum += digit;
 
-
+    } while (number != 0);
+return sum;
+}
+console.log('sum =', sumDigits1(123));
