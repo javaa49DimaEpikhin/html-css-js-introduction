2806d8925c2536a33b2d15432117db9de2bb1eaf
Dima Ipihin <dimaepikhin@gmail.com>
2022-12-01 13:16:11 +0200

    forms

---
+++ src/form.js
@@ -0,0 +1,32 @@
+const inputElements = document.querySelectorAll(".form-class [name]");
+
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log("submitted");
+    const employee = Array.from(inputElements).reduce(
+        (res, cur) => {
+            res[cur.name] = cur.value;
+            return res;
+        }, {}
+    )
+    console.log(employee);
+}
+function onChange(event) {
+    if (event.target.name == "salary") {
+        if (+event.target.value < 1000 || +event.target.value > 40000) {
+            event.target.value = '';
+            error.innerHTML = "<span style='color: red;'>" + "Please enter salary from 1000 to 40000</span>"
+            setTimeout(function () {
+                error.innerHTML = '';
+            }, 5000)
+        }
+
+    }
+
+}
+
+
+
+
+
