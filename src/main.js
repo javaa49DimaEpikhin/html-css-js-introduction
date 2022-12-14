ea3671685f2b415ee1631bbd71eba1a660567e76
Dima Ipihin <dimaepikhin@gmail.com>
2022-12-14 11:50:18 +0200

    HW #24 main Library BookForm

--- src/main.js
+++ src/main.js
@@ -18,9 +18,11 @@ const authorFormInputElements = document.querySelectorAll(".author-form-class [n
 
 const library = new Library();
 
-const bookForm = new BookForm({idForm: "book_form", idPagesInput: "pages_input",
-idDateInput: "date_input", idPagesError: "pages_error", idDateError: "date_error",
-minPages: MIN_PAGES, maxPages: MAX_PAGES, minDate: new Date(minDateString)});
+const bookForm = new BookForm({
+    idForm: "book_form", idPagesInput: "pages_input",
+    idDateInput: "date_input", idPagesError: "pages_error", idDateError: "date_error",
+    minPages: MIN_PAGES, maxPages: MAX_PAGES, minDate: new Date(minDateString)
+});
 bookForm.addSubmitHandler(book => library.addBook(book));
 
 //functions of Pages Form
@@ -29,14 +31,14 @@ let pageTo = 0;
 function onSubmitPages(event) {
     event.preventDefault();
     const books = library.getBooksbyPagesRange(pageFrom, pageTo);
-    booksPagesListElement.innerHTML = getBookItems(books);  
+    booksPagesListElement.innerHTML = getBookItems(books);
 }
 
 function onChangePagesFrom(event) {
     const value = +event.target.value;
     if (pageTo && value >= pageTo) {
         showErrorMessage(event.target, "Page number 'from' must be less than Page number 'to'",
-        pagesFormErrorElement);
+            pagesFormErrorElement);
     } else {
         pageFrom = value;
     }
@@ -45,7 +47,7 @@ function onChangePagesTo(event) {
     const value = +event.target.value;
     if (pageFrom && value < pageFrom) {
         showErrorMessage(event.target, "Page 'To' must be greater than Page 'From'",
-        pagesFormErrorElement);
+            pagesFormErrorElement);
     }
     pageTo = value;
 }
@@ -60,7 +62,7 @@ function showSection(index) {
     }
 }
 function getBookItems(books) {
-    return books.map (book => 
+    return books.map(book =>
         `<li class="books-item">
               <div class="books-item-container">
                  <p class="books-item-paragraph">Title: ${book.book_title} </p>
