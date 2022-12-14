01063bdb328f811f4a88c4daa8337f6c8bfa799a
Dima Ipihin <dimaepikhin@gmail.com>
2022-12-14 11:49:53 +0200

    HW #24 main Library BookForm

--- src/main.js
+++ src/main.js
@@ -1,90 +1,27 @@
 import { Library } from "./data/library.js";
-const bookFormInputElements = document.querySelectorAll(".book-form-class [name]");
-const authorFormInputElements = document.querySelectorAll(".author-form-class [name]");
+import { BookForm } from "./ui/BookForm.js";
+import { showErrorMessage } from "./ui/errorMessage.js";
+
 const MIN_PAGES = 50;
 const MAX_PAGES = 2000;
-const MIN_YEAR = 1980;
-const maxYear = getMaxYear();
-const minDate = new Date('1980-01-01');
-const maxDate = new Date();
-const TIME_OUT_ERROR_MESSAGE = 5000;
-const ERROR_CLASS = "error";
+const minDateString = '1980-01-01';
 const ACTIVE = "active"
 
-
-
-const dateErrorElement = document.getElementById("date_error");
-const pagesErrorElement = document.getElementById("pages_error");
 const pagesFormErrorElement = document.getElementById("pages_form_error");
 const booksListElement = document.getElementById("books-all");
 const booksPagesListElement = document.getElementById("books-pages");
 const booksAuthorListElement = document.getElementById("books-author");
 const sectionsElement = document.querySelectorAll("section");
 const buttonsMenuElement = document.querySelectorAll(".buttons-menu *");
+const authorFormInputElements = document.querySelectorAll(".author-form-class [name]");
+/************************************************************************** */
 
 const library = new Library();
 
-//functions of Book Form
-function onSubmitBookForm(event) {
-    event.preventDefault();
-    console.log("submitted");
-    const book = Array.from(bookFormInputElements).reduce(
-        (res, cur) => {
-            res[cur.name] = cur.value;
-            return res;
-        }, {}
-    )
-    console.log(book)
-    library.addBook(book);
-}
-
-function onChangeBookForm(event) {
-    if (event.target.name == "pages") {
-        validatePages(event.target)
-    } else if (event.target.name == "publishingDate") {
-        validatePublishingDate(event.target);
-        validateDate(event.target);
-    }
-}
-function validatePages(element) {
-    const value = +element.value;
-    if (value < MIN_PAGES || value > MAX_PAGES) {
-        const message = value < MIN_PAGES ? `number of pages must be ${MIN_PAGES} or greater`
-            : `number of pages must be ${MAX_PAGES} or less`;
-        showErrorMessage(element, message, pagesErrorElement);
-    }
-
-}
-function validatePublishingDate(element) {
-    const value = +element.value.slice(0, 4);
-    if (value < MIN_YEAR || value > maxYear) {
-        const message = value < MIN_YEAR ? `year must be ${MIN_YEAR} or greater` :
-            `year must be ${maxYear} or less`;
-        showErrorMessage(element, message, dateErrorElement);
-
-    }
-}
-function validateDate(element) {
-    const selectedDate = new Date(element.value);
-    if (selectedDate < minDate || selectedDate > maxDate) {
-        const message = selectedDate < minDate ? `date must be ${minDate} or greater` :
-            `date must be ${maxDate} or less`;
-        showErrorMessage(element, message, dateErrorElement);
-    }
-}
-function showErrorMessage(element, message, errorElement) {
-    element.classList.add(ERROR_CLASS);
-    errorElement.innerHTML = message;
-    setTimeout(() => {
-        element.classList.remove(ERROR_CLASS);
-        element.value = '';
-        errorElement.innerHTML = '';
-    }, TIME_OUT_ERROR_MESSAGE);
-}
-
-function getMaxYear() {
-    return new Date().getFullYear();
-}
+const bookForm = new BookForm({idForm: "book_form", idPagesInput: "pages_input",
+idDateInput: "date_input", idPagesError: "pages_error", idDateError: "date_error",
+minPages: MIN_PAGES, maxPages: MAX_PAGES, minDate: new Date(minDateString)});
+bookForm.addSubmitHandler(book => library.addBook(book));
 
 //functions of Pages Form
 let pageFrom = 0;
@@ -92,14 +29,14 @@ let pageTo = 0;
 function onSubmitPages(event) {
     event.preventDefault();
     const books = library.getBooksbyPagesRange(pageFrom, pageTo);
-    booksPagesListElement.innerHTML = getBookItems(books);
+    booksPagesListElement.innerHTML = getBookItems(books);  
 }
 
 function onChangePagesFrom(event) {
     const value = +event.target.value;
     if (pageTo && value >= pageTo) {
         showErrorMessage(event.target, "Page number 'from' must be less than Page number 'to'",
-            pagesFormErrorElement);
+        pagesFormErrorElement);
     } else {
         pageFrom = value;
     }
@@ -108,7 +45,7 @@ function onChangePagesTo(event) {
     const value = +event.target.value;
     if (pageFrom && value < pageFrom) {
         showErrorMessage(event.target, "Page 'To' must be greater than Page 'From'",
-            pagesFormErrorElement);
+        pagesFormErrorElement);
     }
     pageTo = value;
 }
@@ -123,7 +60,7 @@ function showSection(index) {
     }
 }
 function getBookItems(books) {
-    return books.map(book =>
+    return books.map (book => 
         `<li class="books-item">
               <div class="books-item-container">
                  <p class="books-item-paragraph">Title: ${book.book_title} </p>
@@ -143,8 +80,6 @@ function onSubmitAuthor(event) {
     booksAuthorListElement.innerHTML = getBookItems(books);
 }
 
-window.onSubmitBookForm = onSubmitBookForm;
-window.onChangeBookForm = onChangeBookForm;
 window.showSection = showSection;
 window.onChangePagesTo = onChangePagesTo;
 window.onChangePagesFrom = onChangePagesFrom;
---
+++ src/ui/BookForm.js
@@ -0,0 +1,69 @@
+import { showErrorMessage } from "./errorMessage.js";
+export class BookForm {
+    #formElement;
+    #inputElements;
+    #dateInputElement;
+    #pageInputElement;
+    #dateErrorElement;
+    #pageErrorElement;
+    #minPages;
+    #maxPages;
+    #minDate;
+    #maxDate;
+    constructor(params) {
+        this.#formElement = document.getElementById(params.idForm);
+        this.#inputElements = document.querySelectorAll(`#${params.idForm} [name]`);
+        this.#dateInputElement = document.getElementById(params.idDateInput);
+        this.#pageInputElement = document.getElementById(params.idPagesInput);
+        this.#dateErrorElement = document.getElementById(params.idDateError);
+        this.#pageErrorElement = document.getElementById(params.idPagesError);
+        this.#minPages = params.minPages;
+        this.#maxPages = params.maxPages;
+        this.#minDate = params.minDate;
+        this.#maxDate = this.getMaxDate();
+        this.onChange();
+    }
+    addSubmitHandler(processBookFun) {
+        this.#formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+            console.log("submitted");
+            const book = Array.from(this.#inputElements).reduce(
+                (res, cur) => {
+                    res[cur.name] = cur.value;
+                    return res;
+                }, {}
+            )
+            console.log(book);
+            processBookFun(book);
+        })
+    }
+    onChange() {
+        this.#dateInputElement.addEventListener("change", (event) => {
+            this.validatePublishingDate(event.target);
+        })
+        this.#pageInputElement.addEventListener("change", (event) => {
+            this.validatePages(event.target);
+        })
+       
+    }
+    validatePages(element) {
+        const value = +element.value;
+        if (value < this.#minPages || value > this.#maxPages) {
+            const message = value < this.#minPages ? `number of pages must be ${this.#minPages} or greater`
+                : `number of pages must be ${this.#maxPages} or less`;
+            showErrorMessage(element, message, this.#pageErrorElement);
+        }
+    }
+    validatePublishingDate(element) {
+        const selectedDate = new Date(element.value);
+        if(selectedDate < this.#minDate || selectedDate > this.#maxDate) {
+            const message = selectedDate < this.#minDate ? `date must be ${this.#minDate} or greater`:
+               `date must be ${this.#maxDate} or less`;
+            showErrorMessage(element, message, this.#dateErrorElement) ;    
+        }
+    }
+        
+    getMaxDate() {
+        return new Date();
+    }
+}
